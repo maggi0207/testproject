cd7dc249c9740998e0fb08d31c0a7ab5183e7691
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-09-18 13:35:31 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,72 +1,213 @@
-import { render, fireEvent } from '@testing-library/react';
-import YourComponent from './YourComponent'; // replace with your actual component
-import { updateLongPollReducer } from './yourReducerFile'; // replace with actual reducer path
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
 
-// Mocking the functions
-jest.mock('./yourReducerFile', () => ({
-    updateLongPollReducer: jest.fn(),
-}));
+import { render, screen } from '../../utils/test-utils/renderHelper';
+import rootReducer from '../../modules/store/reducer';
+import { rootSaga } from '../../modules/store/saga';
+import CheckoutInspicio from './CheckoutInspicio';
+import * as PaymentApiCallHook from '../../modules/services/APIService/PaymentApiCallHooks';
 
-jest.mock('./processReceiveDataFile', () => ({
-    processReceiveData: jest.fn(),
-}));
+describe('CheckoutInspicio Agreement Modal Component', () => {
+  // const mockCloseModalFn = jest.fn();
+  // const handleInspicioToggle = jest.fn();
+  const mockData = {
+    getAllActiveMtnsRequest: jest.fn(),
+    requestBodySendMessage: jest.fn(),
+    handleReceivePollApi: jest.fn(),
+    requestBodySendEmail: jest.fn(),
+    getResultGenerateInspicioId: { data: { sendInspicioToken: 'OMNI', encryptedCartId: '12345' } },
+    getResultSendEmail: { data: { sendEmailStatus: 'SUCCESS' } },
+    getResultSendMessage: { data: { status: 'SENT' } },
+    getAllActiveMtnsResponse: {
+      data: {
+        customer: {
+          billAccounts: [
+            {
+              mtns: [
+                {
+                  mtn: '7125772409',
+                  deviceCategory: 'Smartphone',
+                  mtnStatus: {
+                    isActive: true,
+                    isDisconnected: false,
+                  },
+                },
+              ],
+            },
+            {
+              mtns: [
+                {
+                  mtn: '7125772409',
+                  deviceCategory: 'Smartphone',
+                  mtnStatus: {
+                    isActive: true,
+                    isDisconnected: false,
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  };
 
-describe('YourComponent', () => {
-    let mockUpdateLongPollReducer;
-    let mockProcessReceiveData;
+  const props = {
+    handleInspicioToggle: jest.fn(),
+    selectedEmail: 'test@vrz.com',
+    channel: 'CHAT-STORE',
+    pageName: 'tac',
+    agreementEligibleFlags: {
+      showDeclineEquipmentProtection: false,
+    },
 
-    beforeEach(() => {
-        mockUpdateLongPollReducer = require('./yourReducerFile').updateLongPollReducer;
-        mockProcessReceiveData = require('./processReceiveDataFile').processReceiveData;
-        mockUpdateLongPollReducer.mockClear();
-        mockProcessReceiveData.mockClear();
+    cart: {
+      cartHeader: {
+        fullAccountNumber: '0280083539-1',
+      },
+      orderDetails: {
+        isPrePayCart: 'Y',
+      },
+      lineInfo: [
+        {
+          lineActivityType: 'NSEBYOD',
+        },
+      ],
+    },
+    state: {
+      selectedMtnValue: 'SMS',
+      mtn: [
+        { label: '6102992029', value: '6102992029' },
+        { label: '6102992830', value: '6102992830' },
+        { label: '6103683623', value: '6103683623' },
+        { label: '6106637197', value: '6106637197' },
+      ],
+      checkBoxSelectedValue: true,
+      inspicioModel: false,
+    },
+
+    cBandQualified: true,
+    signatureInspicioFlag: true,
+    inspicioCustomSelectedMTN: 'test',
+    selectedCheckboxValue: true,
+    cancelLongPoll: true,
+  };
+
+  const mockprocessReceiveData = jest.fn();
+
+  jest.mock('./Utils', () => ({
+    processReceiveData: mockprocessReceiveData,
+  }));
+
+  beforeAll(() => { });
+
+
+  beforeEach(() => {
+    // const BPData = agreementSummary?.messages?.backupAgreementDescription;
+    // eslint-disable-next-line no-unused-expressions
+    sessionStorage.getItem('suspended') === 'suspended';
+  });
+
+  test('Close on Agree & Continue button click 0', () => {
+    jest.spyOn(PaymentApiCallHook, 'default').mockImplementation(() => mockData);
+    jest.fn();
+    const Newprops = {
+      onModeChange: jest.fn(),
+      agreementEligibleFlags: {
+        showDeclineEquipmentProtection: false,
+      },
+      cart: {
+        orderDetails: {
+          isPrePayCart: 'N',
+        },
+      },
+
+      channel: 'CHAT-STORE11',
+      cBandQualified: true,
+      signatureInspicioFlag: false,
+      inspicioCustomSelectedMTN: false,
+      selectedCheckboxValue: false,
+      cancelLongPoll: false,
+    };
+    render(<CheckoutInspicio {...Newprops} />, {
+      reducers: rootReducer,
+      sagas: rootSaga,
+    });
+    screen.getByRole('radio', {
+      name: /send sms/i,
     });
 
-    it('should call updateLongPollReducer when receiveData is truthy', () => {
-        // Mock the processReceiveData to return a valid object (truthy value)
-        const receiveData = { someKey: 'someValue' }; // Sample truthy object
-        mockProcessReceiveData.mockReturnValue(receiveData);
+    // const email = 'VQNROOVI70@VZW.COM';
+    const emailValidationInput = screen.getByRole('radio', {
+      name: /send sms/i,
+    });
+    // const emailInput = within(emailValidationInput).getByRole('radio', {
+    //   name: /send sms/i
+    // })
+    fireEvent.click(emailValidationInput);
+    // fireEvent.change(emailValidationInput, { target: { value: '' } });
+    // fireEvent.change(emailValidationInput, { target: { value: email } });
+    // expect(emailValidationInput).toHaveValue(email);
+    screen.logTestingPlaygroundURL();
+  });
 
-        // Mock event data with status 'Success'
-        const eventData = {
-            detail: {
-                status: "Success",
-                event: 'someEvent',
-                payload: 'somePayload'
-            }
-        };
+  test('Close on Agree & Continue button click', () => {
+    jest.spyOn(PaymentApiCallHook, 'default').mockImplementation(() => mockData);
+    render(<CheckoutInspicio {...props} />, {
+      reducers: rootReducer,
+      sagas: rootSaga,
+    });
+  });
 
-        // Render the component
-        render(<YourComponent />);
+  test('Close on Agree & Continue button click 1', () => {
+    jest.spyOn(PaymentApiCallHook, 'default').mockImplementation(() => mockData);
+    // const newProps = {
+    //     ...props,
+    //     signatureInspicioFlag: true,
+    //     inspicioCustomSelectedMTN: true,
+    //     cBandQualified: true,
+    //     cart: {
+    //         orderDetails: {
+    //             isPrePayCart: "N"
+    //         },
+    //         lineInfo: [
+    //             {
+    //                 lineActivityType: "NSE_5G"
+    //             }
+    //         ]
+    //     },
+    //     selectedMtnValue: "SMS"
+    // }
+    render(<CheckoutInspicio {...props} />, {
+      reducers: rootReducer,
+      sagas: rootSaga,
+    });
+    // screen.logTestingPlaygroundURL();
+  });
 
-        // Fire the custom event
-        fireEvent(document, new CustomEvent('receivePoll', { detail: eventData.detail }));
+  test('should handle receivePoll event correctly when status is Success', () => {
+    jest.spyOn(PaymentApiCallHook, 'default').mockImplementation(() => mockData);
+    // Mock event data
+    const eventData = {
+      detail: {
+        status: "Success",
+        event: 'someEvent',
+        payload: 'somePayload'
+      }
+    };
 
-        // Assert that updateLongPollReducer is called with the correct argument
-        expect(mockProcessReceiveData).toHaveBeenCalledWith(eventData.detail.event, eventData.detail.payload);
-        expect(mockUpdateLongPollReducer).toHaveBeenCalledWith(receiveData);
+    const receiveData = { someKey: 'someValue' };
+
+    mockprocessReceiveData.mockReturnValue(receiveData);
+
+    render(<CheckoutInspicio {...props} />, {
+      reducers: rootReducer,
+      sagas: rootSaga,
     });
 
-    it('should not call updateLongPollReducer when receiveData is falsy', () => {
-        // Mock processReceiveData to return null (falsy value)
-        mockProcessReceiveData.mockReturnValue(null);
+    fireEvent(document, new CustomEvent('receivePoll', { detail: eventData.detail }));
+    expect(mockprocessReceiveData).toHaveBeenCalledTimes(1);
 
-        // Mock event data with status 'Success'
-        const eventData = {
-            detail: {
-                status: "Success",
-                event: 'someEvent',
-                payload: 'somePayload'
-            }
-        };
-
-        // Render the component
-        render(<YourComponent />);
-
-        // Fire the custom event
-        fireEvent(document, new CustomEvent('receivePoll', { detail: eventData.detail }));
-
-        // Assert that updateLongPollReducer is not called
-        expect(mockUpdateLongPollReducer).not.toHaveBeenCalled();
-    });
+    // expect(mockUpdateLongPollReducer).toHaveBeenCalledTimes(1);
+  });
 });
