3a89b7adfb48ea3f8dbf720d442139e29218fde8
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-10-16 00:37:31 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
 
+// Calculate the number of selected options
+    const selectedCount = sortedObjectData.reduce(
+      (count, [key]) => (selectedCategory.includes(key) ? count + 1 : count),
+      0
+    );
+
+    // Set the display text
+    const displayText =
+      selectedCount === 0
+        ? 'Select One'
+        : `${selectedCount} option${selectedCount > 1 ? 's' : ''} selected`;
