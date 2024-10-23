293a2f294907731f30927fbbd29a0f627e3de8e5
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-10-23 12:47:21 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,60 @@
+// src/JS/App.js
+
+import { Page, withModel } from '@adobe/aem-react-editable-components';
+import { Provider } from 'react-redux';
+import LoadingIndicator from './components/NextGen/components/LoadingIndicator';
+import store from "./store/index";
+import { setUserActive, setUserInactive } from './redux/userActivitySlice'; // Redux actions for activity
+import { initializeUserActivityListeners } from './utils/userActivityTracker'; // Utility for event tracking
+
+class App extends Page {
+  constructor(props) {
+    super(props);
+    this.activityTimeout = null;
+  }
+
+  componentDidMount() {
+    super.componentDidMount(); // Ensure we call the parent class's componentDidMount()
+
+    // Initialize user activity listeners and dispatch Redux actions
+    this.cleanupListeners = initializeUserActivityListeners(
+      this.handleUserActive.bind(this),
+      this.handleUserInactive.bind(this)
+    );
+  }
+
+  componentWillUnmount() {
+    super.componentWillUnmount(); // Ensure we call the parent class's componentWillUnmount()
+
+    // Cleanup event listeners on unmount
+    if (this.cleanupListeners) {
+      this.cleanupListeners();
+    }
+  }
+
+  handleUserActive() {
+    store.dispatch(setUserActive()); // Dispatch Redux action for user active
+  }
+
+  handleUserInactive() {
+    store.dispatch(setUserInactive()); // Dispatch Redux action for user inactive
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        {this.childComponents} 
+        {this.childPages}
+        <LoadingIndicator />
+      </Provider>
+    );
+  }
+}
+
+export default withModel(App);
+
+
+
 // userActivitySlice.js
 const initialState = {
   isUserActive: true,
