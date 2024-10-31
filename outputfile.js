d48f5a28ea92c9bb21209229c6e91d5c0fdc3662
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-10-31 09:44:55 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,69 +1,68 @@
-npm install --save-dev webpack webpack-cli webpack-dev-server babel-loader @babel/core @babel/preset-env @babel/preset-react html-webpack-plugin css-loader style-loader
+import React from 'react';
+import PropTypes from 'prop-types';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import withTheme from './withTheme';
 
+// MockComponent to wrap with HOC
+const MockComponent = ({ surface, color, className, children }) => (
+  <div data-testid="mock-component" className={className} style={{ backgroundColor: color }}>
+    {children}
+  </div>
+);
 
-//webpack.config.js
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, 'dist'),
-    open: true,
-    compress: true,
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' },
-      },
-    },
-  },
+MockComponent.propTypes = {
+  surface: PropTypes.string,
+  color: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node,
 };
 
-// client/config/babel/babel.config.js
+// Wrapped MockComponent with HOC
+const ThemedMockComponent = withTheme(MockComponent);
 
-module.exports = {
-  presets: [
-    '@babel/preset-env', 
-    '@babel/preset-react', 
-  ],
-  plugins: [
-    '@babel/plugin-transform-runtime',
-  ],
-};
+describe('withTheme HOC', () => {
+  beforeEach(() => {
+    // Reset the window.mfe values before each test
+    delete window.mfe;
+  });
+
+  test('should render wrapped component with light theme when isDark is false', () => {
+    render(<ThemedMockComponent color="#000000" className="light-theme">Light Theme</ThemedMockComponent>);
+
+    const component = screen.getByTestId('mock-component');
+    expect(component).toHaveClass('light-theme');
+    expect(component).toHaveStyle('background-color: #000000');
+    expect(component).toHaveTextContent('Light Theme');
+  });
+
+  test('should render wrapped component with dark theme when isDark is true', () => {
+    // Set up global values for dark theme
+    window.mfe = { scmDeviceMfeEnable: true, isDark: true };
+
+    render(<ThemedMockComponent color="#000000" className="light-theme">Dark Theme</ThemedMockComponent>);
+
+    const component = screen.getByTestId('mock-component');
+    expect(component).toHaveClass('dark-theme');
+    expect(component).toHaveStyle('background-color: #ffffff');
+    expect(component).toHaveTextContent('Dark Theme');
+  });
+
+  test('should apply correct class and color when className and color props are provided', () => {
+    render(<ThemedMockComponent color="#123456" className="custom-class">Custom Props</ThemedMockComponent>);
+
+    const component = screen.getByTestId('mock-component');
+    expect(component).toHaveClass('custom-class');
+    expect(component).toHaveStyle('background-color: #123456');
+    expect(component).toHaveTextContent('Custom Props');
+  });
+
+  test('should pass through additional props', () => {
+    const handleOpenChange = jest.fn();
+    render(<ThemedMockComponent color="#abcdef" onOpenedChange={handleOpenChange}>Extra Props</ThemedMockComponent>);
+
+    const component = screen.getByTestId('mock-component');
+    expect(component).toHaveStyle('background-color: #abcdef');
+    expect(component).toHaveTextContent('Extra Props');
+  });
+});
