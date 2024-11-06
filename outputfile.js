9a5a0ec4669d97836375af01c5a79c164103acf1
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-05 23:47:18 -0700

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,1 +1,27 @@
-npm install $(npm outdated | cut -d' ' -f 1 | sed '1d' | xargs -I '$' echo '$@latest' | xargs echo)
+{
+    "title": "Case email notification",
+    "description": "Select a case for case conformation and random",
+    "primarySubTitle": "Daily summary",
+    "primarySubDescription": "Recieving the text from rte",
+    "checkboxDescription": "Instant notification for cases",
+    "secondarySubTitle": "Instant",
+    "secondarySubDescription": "recieved email",
+    "cqType": "ECCHub/components/nextgen/caseemailnotification",
+    "cqPath": "/content/ECCHub/us/en/my-account/jcr:content/root/responsivegrid/container/responsivegrid/caseemailnotificatio",
+    "isInEditor": false,
+    "componentProperties": {
+        "title": "Case email notification",
+        "description": "Select a case for case conformation and random",
+        "primarySubTitle": "Daily summary",
+        "primarySubDescription": "Recieving the text from rte",
+        "checkboxDescription": "Instant notification for cases",
+        "secondarySubTitle": "Instant",
+        "secondarySubDescription": "recieved email",
+        "cqType": "ECCHub/components/nextgen/caseemailnotification",
+        "cqPath": "/content/ECCHub/us/en/my-account/jcr:content/root/responsivegrid/container/responsivegrid/caseemailnotificatio",
+        "isInEditor": false,
+        "containerProps": {
+            "className": " aem-GridColumn aem-GridColumn--default--12"
+        }
+    }
+}
