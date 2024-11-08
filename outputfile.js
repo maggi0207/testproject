8c0ee5cbe970a90a40fa465003c9d28ba0d6cd76
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-08 09:09:18 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,27 +1,10 @@
-{
-    "title": "Case email notification",
-    "description": "Select a case for case conformation and random",
-    "primarySubTitle": "Daily summary",
-    "primarySubDescription": "Recieving the text from rte",
-    "checkboxDescription": "Instant notification for cases",
-    "secondarySubTitle": "Instant",
-    "secondarySubDescription": "recieved email",
-    "cqType": "ECCHub/components/nextgen/caseemailnotification",
-    "cqPath": "/content/ECCHub/us/en/my-account/jcr:content/root/responsivegrid/container/responsivegrid/caseemailnotificatio",
-    "isInEditor": false,
-    "componentProperties": {
-        "title": "Case email notification",
-        "description": "Select a case for case conformation and random",
-        "primarySubTitle": "Daily summary",
-        "primarySubDescription": "Recieving the text from rte",
-        "checkboxDescription": "Instant notification for cases",
-        "secondarySubTitle": "Instant",
-        "secondarySubDescription": "recieved email",
-        "cqType": "ECCHub/components/nextgen/caseemailnotification",
-        "cqPath": "/content/ECCHub/us/en/my-account/jcr:content/root/responsivegrid/container/responsivegrid/caseemailnotificatio",
-        "isInEditor": false,
-        "containerProps": {
-            "className": " aem-GridColumn aem-GridColumn--default--12"
-        }
-    }
+/* CommunicationNotificationPreferences.scss */
+.custom-switch {
+  & .MuiSwitch-switchBase.Mui-checked {
+    color: green; /* Ball color when checked */
+  }
+
+  & .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track {
+    background-color: green; /* Track color when checked */
+  }
 }
