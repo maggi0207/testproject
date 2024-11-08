965e51d1977af1e05ca63177b8070695864c493e
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-08 09:17:59 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,10 +1,13 @@
-/* CommunicationNotificationPreferences.scss */
-.custom-switch {
-  & .MuiSwitch-switchBase.Mui-checked {
-    color: green; /* Ball color when checked */
-  }
-
-  & .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track {
-    background-color: green; /* Track color when checked */
-  }
-}
+ <Switch
+                  checked={webNotifications}
+                  onChange={() => setWebNotifications(!webNotifications)}
+                  color="primary"
+                  sx={{
+                    '& .MuiSwitch-switchBase.Mui-checked': {
+                      color: 'green', // Ball color when checked
+                    },
+                    '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+                      backgroundColor: 'green', // Track color when checked
+                    },
+                  }}
+                />
