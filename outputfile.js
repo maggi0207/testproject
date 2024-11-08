5c599ea5e0511a1d4d8b94267fdc51ba7a5e196c
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-08 09:47:47 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,83 +1,49 @@
-/* CommunicationNotificationPreferences.scss */
-.custom-switch  {
-  width: 42px;
-  height: 26px;
-  padding: 0;
-
-  .MuiSwitch-switchBase {
-    padding: 0;
-    margin: 2px;
-    transition-duration: 300ms;
-
-    &.Mui-checked {
-      transform: translateX(16px);
-      color: #fff;
-
-      & + .MuiSwitch-track {
-        background-color: #65c466;
-        opacity: 1;
-        border: 0;
-      }
-
-      &.Mui-focusVisible + .MuiSwitch-track {
-        background-color: #2eca45;
-      }
-
-      &.Mui-disabled + .MuiSwitch-track {
-        opacity: 0.5;
-      }
-    }
-
-    &.Mui-focusVisible .MuiSwitch-thumb {
-      color: #33cf4d;
-      border: 6px solid #fff;
-    }
-
-    &.Mui-disabled .MuiSwitch-thumb {
-      color: #e0e0e0; // gray equivalent for disabled state
-    }
-
-    &.Mui-disabled + .MuiSwitch-track {
-      opacity: 0.7;
-    }
-  }
-
-  .MuiSwitch-thumb {
-    box-sizing: border-box;
-    width: 22px;
-    height: 22px;
-  }
-
-  .MuiSwitch-track {
-    border-radius: 50%;
-    background-color: #e9e9ea;
-    opacity: 1;
-    transition: background-color 500ms ease-in-out;
-
-    &.Mui-checked {
-      background-color: #65c466;
-    }
-  }
-
-  &.dark {
-    .MuiSwitch-switchBase {
-      &.Mui-checked {
-        & + .MuiSwitch-track {
-          background-color: #2eca45;
-        }
-      }
-
-      &.Mui-disabled + .MuiSwitch-track {
-        opacity: 0.3;
-      }
-    }
-
-    .MuiSwitch-track {
-      background-color: #39393d;
-    }
-
-    .MuiSwitch-thumb {
-      color: #757575; // Grey for dark mode disabled thumb
-    }
-  }
-}
+const AntSwitch = withStyles((theme) => ({
+  root: {
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex',
+    '&:active': {
+      '& $thumb': {
+        width: 15,
+      },
+      '& $switchBase.Mui-checked': {
+        transform: 'translateX(9px)',
+      },
+    },
+  },
+  switchBase: {
+    padding: 2,
+    '&$checked': {
+      transform: 'translateX(12px)',
+      color: '#fff',
+      '& + $track': {
+        opacity: 1,
+        backgroundColor: '#1890ff',
+        ...theme.palette.type === 'dark' && {
+          backgroundColor: '#177ddc',
+        },
+      },
+    },
+  },
+  thumb: {
+    boxShadow: '0 2px 4px 0 rgba(0, 35, 11, 0.2)',
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    transition: theme.transitions.create(['width'], {
+      duration: 200,
+    }),
+  },
+  track: {
+    borderRadius: 8,
+    opacity: 1,
+    backgroundColor: 'rgba(0,0,0,.25)',
+    boxSizing: 'border-box',
+    ...theme.palette.type === 'dark' && {
+      backgroundColor: 'rgba(255,255,255,.35)',
+    },
+  },
+  checked: {},
+}))(Switch);
