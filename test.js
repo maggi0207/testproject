022b1cab0bc975a61ee8f17e2d4de8d54796a5f3
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-11 02:37:28 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,44 @@
-const CommunicationDistributionListMembers = ({ emailNotifications, setEmailNotifications, webNotifications, setWebNotifications }) => {
+// notificationSlice.js
+import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+    initialValues: {
+        dailySummary: true,
+        instantNotification: false,
+        emailNotification: true,
+        webNotification: true,
+    },
+    currentValues: {
+        dailySummary: true,
+        instantNotification: false,
+        emailNotification: true,
+        webNotification: true,
+    },
+    isModified: false, // Track if any toggle has changed
+};
 
-    const EmailDeliveryPreferences = ({ dailySummary, setDailySummary, instantNotification, setInstantNotification, pendingActionNotification, setPendingActionNotification }) => {
- 
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState,
+    reducers: {
+        setInitialValues: (state, action) => {
+            state.initialValues = action.payload;
+            state.currentValues = action.payload;
+            state.isModified = false;
+        },
+        updateToggleValue: (state, action) => {
+            const { key, value } = action.payload;
+            state.currentValues[key] = value;
+            state.isModified = Object.keys(state.initialValues).some(
+                (key) => state.initialValues[key] !== state.currentValues[key]
+            );
+        },
+        resetValues: (state) => {
+            state.currentValues = state.initialValues;
+            state.isModified = false;
+        },
+    },
+});
+
+export const { setInitialValues, updateToggleValue, resetValues } = notificationSlice.actions;
+export default notificationSlice.reducer;
