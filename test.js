e8f24b9972bf3f4f383b514011300fd9aef0db77
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-11 02:46:12 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,43 +2,60 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    initialValues: {
-        dailySummary: true,
-        instantNotification: false,
-        emailNotification: true,
-        webNotification: true,
+    emailPreferences: {
+        initial: 'normal', // Set initial to 'normal'
+        current: 'normal', // Set current to 'normal'
     },
-    currentValues: {
-        dailySummary: true,
-        instantNotification: false,
-        emailNotification: true,
-        webNotification: true,
+    communicationPreferences: {
+        initial: 'email', // Set initial to 'email'
+        current: 'email', // Set current to 'email'
     },
-    isModified: false, // Track if any toggle has changed
+    isModified: false,
 };
 
 const notificationSlice = createSlice({
     name: 'notification',
     initialState,
     reducers: {
-        setInitialValues: (state, action) => {
-            state.initialValues = action.payload;
-            state.currentValues = action.payload;
+        setEmailPreferencesInitialValues: (state, action) => {
+            state.emailPreferences.initial = action.payload;
+            state.emailPreferences.current = action.payload;
             state.isModified = false;
         },
-        updateToggleValue: (state, action) => {
-            const { key, value } = action.payload;
-            state.currentValues[key] = value;
-            state.isModified = Object.keys(state.initialValues).some(
-                (key) => state.initialValues[key] !== state.currentValues[key]
-            );
+        setCommunicationPreferencesInitialValues: (state, action) => {
+            state.communicationPreferences.initial = action.payload;
+            state.communicationPreferences.current = action.payload;
+            state.isModified = false;
+        },
+        updateEmailPreferenceToggle: (state, action) => {
+            const { value } = action.payload;
+            state.emailPreferences.current = value;
+            state.isModified = checkIfModified(state);
+        },
+        updateCommunicationPreferenceToggle: (state, action) => {
+            const { value } = action.payload;
+            state.communicationPreferences.current = value;
+            state.isModified = checkIfModified(state);
         },
         resetValues: (state) => {
-            state.currentValues = state.initialValues;
+            state.emailPreferences.current = state.emailPreferences.initial;
+            state.communicationPreferences.current = state.communicationPreferences.initial;
             state.isModified = false;
         },
     },
 });
 
-export const { setInitialValues, updateToggleValue, resetValues } = notificationSlice.actions;
+const checkIfModified = (state) => {
+    return state.emailPreferences.initial !== state.emailPreferences.current ||
+           state.communicationPreferences.initial !== state.communicationPreferences.current;
+};
+
+export const {
+    setEmailPreferencesInitialValues,
+    setCommunicationPreferencesInitialValues,
+    updateEmailPreferenceToggle,
+    updateCommunicationPreferenceToggle,
+    resetValues,
+} = notificationSlice.actions;
+
 export default notificationSlice.reducer;
