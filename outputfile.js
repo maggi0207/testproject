b9ebc3e6d5f750b884bdd2b1399103b32c3a0f87
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-18 11:28:40 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,9 +1,9 @@
-
-import {MapTo} from '@adobe/aem-react-editable-components';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert'; // For MUI v4.7
 import {ToggleSwitch} from '../Togglewitch';
 import getDistributionListMembers from '../../../../Services/getDistributionListMembers';
-import {useDispatch, useSelector} from 'react-redux';
 import {
     resetValues,
     setCommunicationPreferencesInitialValues,
@@ -13,27 +13,21 @@ import {
 import updateEmailDeliveryPreferences from '../../../../Services/updateEmailDeliveryPreferences';
 import {loadingIndicatorActions} from '../../../../store/LoadingIndicator/LoadingIndicatorSlice';
 import {getPreferenceFromBooleans} from "../../../../utils/communicationNotificationUtils";
-import updateDistributionListMembers from "../../../../Services/updateDistributionListMembers ";
+import updateDistributionListMembers from "../../../../Services/updateDistributionListMembers";
 import ReactTooltip from 'react-tooltip';
 
-const CommunicationMessageNotificationsConfig = {
-    emptyLabel: 'CCH Communication Message Notification',
-
-    isEmpty: function (props) {
-        return !props || !props.heading;
-    },
-};
-
+// Alert wrapper for Snackbar
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const CommunicationDistributionListMembers = (props) => {
+    const [toast, setToast] = useState({open: false, message: '', severity: 'success'}); // Toast state
+
     const currentEmailPreference = useSelector((state) => state.emailPreferenceSlice.emailPreferences.current);
     const communicationPreferences = useSelector((state) => state.emailPreferenceSlice.communicationPreferences.current);
-
     const isModified = useSelector((state) => state.emailPreferenceSlice.isModified);
-
-
     const dispatch = useDispatch();
-
 
     useEffect(() => {
         dispatch(loadingIndicatorActions.setLoadingIndicatorProps({isLoading: true}));
@@ -55,8 +49,6 @@ const CommunicationDistributionListMembers = (props) => {
     const updateToggleStatus = (deliveryMethod) => {
         switch (deliveryMethod) {
             case "Email":
-                dispatch(setCommunicationPreferencesInitialValues({emailNotifications: true}));
-                break;
             case "Email;ECC Hub Notification":
                 dispatch(setCommunicationPreferencesInitialValues({emailNotifications: true}));
                 break;
@@ -64,8 +56,7 @@ const CommunicationDistributionListMembers = (props) => {
                 dispatch(setCommunicationPreferencesInitialValues({emailNotifications: false}));
                 break;
         }
-    }
-
+    };
 
     const submitButtonHandler = async () => {
         const currentPreference = getPreferenceFromBooleans(currentEmailPreference.dailySummary, currentEmailPreference.instantNotification, currentEmailPreference.pendingActionNotification);
@@ -76,24 +67,34 @@ const CommunicationDistributionListMembers = (props) => {
             await updateEmailDeliveryPreferences(currentPreference);
             await updateDistributionListMembers(deliveryMethod);
             dispatch(setCurrentPreferencesInitialValues());
+            setToast({open: true, message: 'Preferences updated successfully!', severity: 'success'}); // Success toast
         } catch (error) {
             console.error("Error occurred:", error);
+            setToast({open: true, message: 'Failed to update preferences.', severity: 'error'}); // Failure toast
         } finally {
             dispatch(loadingIndicatorActions.setLoadingIndicatorProps({isLoading: false}));
         }
-    }
+    };
 
     const cancelButtonHandler = () => {
         dispatch(resetValues());
-    }
+        setToast({open: true, message: 'Preferences reset successfully.', severity: 'warning'}); // Warning toast
+    };
 
     const handleToggleChange = () => {
-        dispatch((updateCommunicationPreferenceToggle({emailNotifications: !communicationPreferences?.emailNotifications})));
-    }
+        dispatch(updateCommunicationPreferenceToggle({emailNotifications: !communicationPreferences?.emailNotifications}));
+    };
+
+    const handleCloseToast = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setToast({...toast, open: false});
+    };
 
     return (
         <>
-            {props?.preferenceEmail && props?.preferenceToolTip ?
+            {props?.preferenceEmail && props?.preferenceToolTip ? (
                 <div className="communication-preferences-container">
                     <div className="communication-preferences-paper">
                         <h4 className="notification__title">{props?.titlePreferences}</h4>
@@ -104,19 +105,17 @@ const CommunicationDistributionListMembers = (props) => {
                                 <div className="preference-label">
                                     <div>
                                         {props?.preferenceName}
-                                        <spn>
-                                            <i data-tip data-for="preference" className="iconCircleInfo">
-                                            </i>
-                                        </spn>
-                                        <ReactTooltip id="preference" place='top' backgroundColor='#001D5B'
-                                                      arrowColor='#001D5B' textColor='#fff' borderColor='001D5B'
-                                                      multiline={true} className='opt_tooltip'>
+                                        <span>
+                                            <i data-tip data-for="preference" className="iconCircleInfo"></i>
+                                        </span>
+                                        <ReactTooltip id="preference" place="top" backgroundColor="#001D5B"
+                                                      arrowColor="#001D5B" textColor="#fff" borderColor="001D5B"
+                                                      multiline={true} className="opt_tooltip">
                                             {props?.preferenceToolTip}
                                         </ReactTooltip>
                                     </div>
                                 </div>
                             </div>
-
                             <div className="toggle-group">
                                 <div className="email-notification">{props?.preferenceEmail}</div>
                                 <div className="toggle-control">
@@ -132,17 +131,25 @@ const CommunicationDistributionListMembers = (props) => {
                         </div>
                     </div>
                 </div>
-                : <></>}
-            <div className='notification-update-button-container'>
-                <div className='button-row'>
-                    <button className="btn-primary" disabled={!isModified} type="button"
-                            onClick={() => submitButtonHandler()}>Submit</button>
-                    <button className="btn-secondary" disabled={!isModified} type="button"
-                            onClick={() => cancelButtonHandler()}>Cancel</button>
+            ) : <></>}
+            <div className="notification-update-button-container">
+                <div className="button-row">
+                    <button className="btn-primary" disabled={!isModified} type="button" onClick={submitButtonHandler}>
+                        Submit
+                    </button>
+                    <button className="btn-secondary" disabled={!isModified} type="button" onClick={cancelButtonHandler}>
+                        Cancel
+                    </button>
                 </div>
             </div>
+            {/* Snackbar for toast notifications */}
+            <Snackbar open={toast.open} autoHideDuration={6000} onClose={handleCloseToast}>
+                <Alert onClose={handleCloseToast} severity={toast.severity}>
+                    {toast.message}
+                </Alert>
+            </Snackbar>
         </>
-
     );
-}
-export default MapTo('ECCHub/components/nextgen/communicationmessagenotifications')(CommunicationDistributionListMembers, CommunicationMessageNotificationsConfig);
+};
+
+export default CommunicationDistributionListMembers;
