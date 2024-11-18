7dfac8acb3a9d4266d606b3de5008e3a94bf76e8
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-18 11:48:21 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,9 +1,8 @@
-import React, {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert'; // For MUI v4.7
-import {ToggleSwitch} from '../Togglewitch';
+import { MapTo } from '@adobe/aem-react-editable-components';
+import React, { useState, useEffect } from 'react';
+import { ToggleSwitch } from '../Togglewitch';
 import getDistributionListMembers from '../../../../Services/getDistributionListMembers';
+import { useDispatch, useSelector } from 'react-redux';
 import {
     resetValues,
     setCommunicationPreferencesInitialValues,
@@ -11,85 +10,94 @@ import {
     updateCommunicationPreferenceToggle
 } from '../../../../store/CommunicationNotification/EmailPreferenceSlice';
 import updateEmailDeliveryPreferences from '../../../../Services/updateEmailDeliveryPreferences';
-import {loadingIndicatorActions} from '../../../../store/LoadingIndicator/LoadingIndicatorSlice';
-import {getPreferenceFromBooleans} from "../../../../utils/communicationNotificationUtils";
-import updateDistributionListMembers from "../../../../Services/updateDistributionListMembers";
+import { loadingIndicatorActions } from '../../../../store/LoadingIndicator/LoadingIndicatorSlice';
+import { getPreferenceFromBooleans } from '../../../../utils/communicationNotificationUtils';
+import updateDistributionListMembers from '../../../../Services/updateDistributionListMembers';
 import ReactTooltip from 'react-tooltip';
+import { Snackbar, Box, Typography } from '@material-ui/core'; // Use Snackbar and Material-UI components
 
-// Alert wrapper for Snackbar
-function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
+const CommunicationMessageNotificationsConfig = {
+    emptyLabel: 'CCH Communication Message Notification',
+
+    isEmpty: function (props) {
+        return !props || !props.heading;
+    },
+};
 
 const CommunicationDistributionListMembers = (props) => {
-    const [toast, setToast] = useState({open: false, message: '', severity: 'success'}); // Toast state
+    const [alert, setAlert] = useState({ open: false, severity: 'success', message: '' });
 
     const currentEmailPreference = useSelector((state) => state.emailPreferenceSlice.emailPreferences.current);
     const communicationPreferences = useSelector((state) => state.emailPreferenceSlice.communicationPreferences.current);
+
     const isModified = useSelector((state) => state.emailPreferenceSlice.isModified);
+
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(loadingIndicatorActions.setLoadingIndicatorProps({isLoading: true}));
-        getDistributionListMembers().then(response => {
-            dispatch(loadingIndicatorActions.setLoadingIndicatorProps({isLoading: false}));
-            if (response.records && response.records.length > 0) {
-                const deliveryMethod = response.records[0].Delivery_Method__c;
-                updateToggleStatus(deliveryMethod);
-            } else {
-                console.log('No records found:', response);
-                updateToggleStatus(null);
-            }
-        }).catch(error => {
-            dispatch(loadingIndicatorActions.setLoadingIndicatorProps({isLoading: false}));
-            console.error('Error fetching distribution list members:', error);
-        });
+        dispatch(loadingIndicatorActions.setLoadingIndicatorProps({ isLoading: true }));
+        getDistributionListMembers()
+            .then((response) => {
+                dispatch(loadingIndicatorActions.setLoadingIndicatorProps({ isLoading: false }));
+                if (response.records && response.records.length > 0) {
+                    const deliveryMethod = response.records[0].Delivery_Method__c;
+                    updateToggleStatus(deliveryMethod);
+                } else {
+                    console.log('No records found:', response);
+                    updateToggleStatus(null);
+                }
+            })
+            .catch((error) => {
+                dispatch(loadingIndicatorActions.setLoadingIndicatorProps({ isLoading: false }));
+                console.error('Error fetching distribution list members:', error);
+            });
     }, [dispatch]);
 
     const updateToggleStatus = (deliveryMethod) => {
         switch (deliveryMethod) {
-            case "Email":
-            case "Email;ECC Hub Notification":
-                dispatch(setCommunicationPreferencesInitialValues({emailNotifications: true}));
+            case 'Email':
+            case 'Email;ECC Hub Notification':
+                dispatch(setCommunicationPreferencesInitialValues({ emailNotifications: true }));
                 break;
             default:
-                dispatch(setCommunicationPreferencesInitialValues({emailNotifications: false}));
+                dispatch(setCommunicationPreferencesInitialValues({ emailNotifications: false }));
                 break;
         }
     };
 
     const submitButtonHandler = async () => {
-        const currentPreference = getPreferenceFromBooleans(currentEmailPreference.dailySummary, currentEmailPreference.instantNotification, currentEmailPreference.pendingActionNotification);
-        const deliveryMethod = communicationPreferences?.emailNotifications ? "Email" : "None";
-        dispatch(loadingIndicatorActions.setLoadingIndicatorProps({isLoading: true}));
+        const currentPreference = getPreferenceFromBooleans(
+            currentEmailPreference.dailySummary,
+            currentEmailPreference.instantNotification,
+            currentEmailPreference.pendingActionNotification
+        );
+        const deliveryMethod = communicationPreferences?.emailNotifications ? 'Email' : 'None';
+        dispatch(loadingIndicatorActions.setLoadingIndicatorProps({ isLoading: true }));
 
         try {
             await updateEmailDeliveryPreferences(currentPreference);
             await updateDistributionListMembers(deliveryMethod);
             dispatch(setCurrentPreferencesInitialValues());
-            setToast({open: true, message: 'Preferences updated successfully!', severity: 'success'}); // Success toast
+            setAlert({ open: true, severity: 'success', message: 'Your preferences have been successfully updated.' });
         } catch (error) {
-            console.error("Error occurred:", error);
-            setToast({open: true, message: 'Failed to update preferences.', severity: 'error'}); // Failure toast
+            console.error('Error occurred:', error);
+            setAlert({ open: true, severity: 'error', message: 'Failed to update preferences. Please try again.' });
         } finally {
-            dispatch(loadingIndicatorActions.setLoadingIndicatorProps({isLoading: false}));
+            dispatch(loadingIndicatorActions.setLoadingIndicatorProps({ isLoading: false }));
         }
     };
 
     const cancelButtonHandler = () => {
         dispatch(resetValues());
-        setToast({open: true, message: 'Preferences reset successfully.', severity: 'warning'}); // Warning toast
+        setAlert({ open: true, severity: 'error', message: 'Preferences reset. No changes were saved.' });
     };
 
     const handleToggleChange = () => {
-        dispatch(updateCommunicationPreferenceToggle({emailNotifications: !communicationPreferences?.emailNotifications}));
+        dispatch(updateCommunicationPreferenceToggle({ emailNotifications: !communicationPreferences?.emailNotifications }));
     };
 
-    const handleCloseToast = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        setToast({...toast, open: false});
+    const handleCloseAlert = () => {
+        setAlert({ ...alert, open: false });
     };
 
     return (
@@ -101,21 +109,35 @@ const CommunicationDistributionListMembers = (props) => {
                         <p className="notification__desc">{props?.descriptionPreferences}</p>
                         <div className="preference-row">
                             <div className="toggle-group">
-                                <div className="preference-label"><strong>{props?.preferenceText}</strong></div>
+                                <div className="preference-label">
+                                    <strong>{props?.preferenceText}</strong>
+                                </div>
                                 <div className="preference-label">
                                     <div>
                                         {props?.preferenceName}
-                                        <span>
-                                            <i data-tip data-for="preference" className="iconCircleInfo"></i>
-                                        </span>
-                                        <ReactTooltip id="preference" place="top" backgroundColor="#001D5B"
-                                                      arrowColor="#001D5B" textColor="#fff" borderColor="001D5B"
-                                                      multiline={true} className="opt_tooltip">
+                                        <spn>
+                                            <i
+                                                data-tip
+                                                data-for="preference"
+                                                className="iconCircleInfo"
+                                            ></i>
+                                        </spn>
+                                        <ReactTooltip
+                                            id="preference"
+                                            place="top"
+                                            backgroundColor="#001D5B"
+                                            arrowColor="#001D5B"
+                                            textColor="#fff"
+                                            borderColor="001D5B"
+                                            multiline={true}
+                                            className="opt_tooltip"
+                                        >
                                             {props?.preferenceToolTip}
                                         </ReactTooltip>
                                     </div>
                                 </div>
                             </div>
+
                             <div className="toggle-group">
                                 <div className="email-notification">{props?.preferenceEmail}</div>
                                 <div className="toggle-control">
@@ -131,25 +153,52 @@ const CommunicationDistributionListMembers = (props) => {
                         </div>
                     </div>
                 </div>
-            ) : <></>}
+            ) : (
+                <></>
+            )}
             <div className="notification-update-button-container">
                 <div className="button-row">
-                    <button className="btn-primary" disabled={!isModified} type="button" onClick={submitButtonHandler}>
+                    <button
+                        className="btn-primary"
+                        disabled={!isModified}
+                        type="button"
+                        onClick={() => submitButtonHandler()}
+                    >
                         Submit
                     </button>
-                    <button className="btn-secondary" disabled={!isModified} type="button" onClick={cancelButtonHandler}>
+                    <button
+                        className="btn-secondary"
+                        disabled={!isModified}
+                        type="button"
+                        onClick={() => cancelButtonHandler()}
+                    >
                         Cancel
                     </button>
                 </div>
             </div>
-            {/* Snackbar for toast notifications */}
-            <Snackbar open={toast.open} autoHideDuration={6000} onClose={handleCloseToast}>
-                <Alert onClose={handleCloseToast} severity={toast.severity}>
-                    {toast.message}
-                </Alert>
+
+            {/* Snackbar Alert */}
+            <Snackbar
+                open={alert.open}
+                autoHideDuration={6000}
+                onClose={handleCloseAlert}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            >
+                <Box
+                    bgcolor={alert.severity === 'success' ? 'green' : 'red'}
+                    color="white"
+                    px={2}
+                    py={1}
+                    borderRadius="4px"
+                >
+                    <Typography variant="body2">{alert.message}</Typography>
+                </Box>
             </Snackbar>
         </>
     );
 };
 
-export default CommunicationDistributionListMembers;
+export default MapTo('ECCHub/components/nextgen/communicationmessagenotifications')(
+    CommunicationDistributionListMembers,
+    CommunicationMessageNotificationsConfig
+);
