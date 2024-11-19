f6926ebf330bd7972ded3eb92c46ea0a4145cd9a
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-18 23:00:59 -0700

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,20 +1,62 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import { ToggleSwitch } from './ToggleSwitch';  // Path to the ToggleSwitch component
+import React from "react";
+import {Snackbar, Box, Typography, Grid} from "@material-ui/core";
 
-describe('ToggleSwitch Component', () => {
+const ToastNotification = ({open, handleClose, message, type, heading}) => {
+    return (
+        <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            anchorOrigin={{vertical: "top", horizontal: "right"}}
+        >
+            <Box
+                display="flex"
+                flexDirection="column"
+                p={2}
+                gap={9}
+                width="292px"
+                height="auto"
+                top="390px"
+                bgcolor={type === "success" ? "#EFF6EF" : "#E9F1FF"}
+                border={`1px solid ${type === "success" ? "#007000" : "#0C55B8"}`}
+                boxShadow="0px 4px 8px 0px #19191A29"
+                borderRadius="8px"
+            >
+                <Grid container spacing={1}>
+                    <Grid item>
+                        <Box style={{marginTop: "5px", marginRight: "2px"}}>
+                            {type === "success" ? (
+                                <span className="Toast_Positive"></span>
 
-  test('should render the ToggleSwitch component correctly', () => {
-    render(<ToggleSwitch checked={false} onChange={() => {}} label="Toggle switch" />);
-    const switchElement = screen.getByRole('switch');  // Query by role
-    expect(switchElement).toBeInTheDocument();
-  });
+                            ) : (
+                                <span className="Toast_Activity"></span>
+                            )}
+                        </Box>
+                    </Grid>
 
-  test('should call onChange when the switch is clicked', () => {
-    const onChangeMock = jest.fn();
-    render(<ToggleSwitch checked={false} onChange={onChangeMock} label="Toggle switch" />);
-    const switchElement = screen.getByRole('switch');  // Query by role
-    fireEvent.click(switchElement);  // Simulate click on the switch
-    expect(onChangeMock).toHaveBeenCalledTimes(1);
-  });
-});
+
+                    <Grid item xs>
+                        <Typography
+                            variant="h6"
+                            style={{
+                                fontWeight: "bold",
+                                color: type === "success" ? "#007000" : "#323334",
+                                fontSize: "16px",
+                            }}
+                        >
+                            {heading || (type === "success" ? "Success" : "Failure")}
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            style={{color: "#19191A"}}
+                        >
+                            {message}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Snackbar>
+    );
+};
+
+export default ToastNotification;
