8ea9585d26bc1d81e9f487323b4ab66e0df1ba0d
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-25 18:02:20 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,3 +1,7 @@
 jest.mock("../../actions/paymentdataset.actions", () => ({
   fetchAllPaymentDatasetAction: jest.fn(),
 }));
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: jest.fn(),
+}))
