113c2860a7148ff1b5dad5f1693698fe91dea535
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-11-26 17:59:23 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,7 +1,25 @@
-jest.mock("../../actions/paymentdataset.actions", () => ({
-  fetchAllPaymentDatasetAction: jest.fn(),
+jest.mock('../../../service/api.service', () => ({
+  apiCall: (
+    url,
+    method,
+    data,
+    headers,
+    includeJsonHeader = true,
+    includeFormDataHeader = false
+  ) => {
+    if (url.includes("api/v1/payments/datasets/all")) {
+      return Promise.resolve({
+        items: [
+          {
+            id: "673d09c874e7d05da2ec7fc0",
+            name: "sai_epods_test_128239923",
+            sourceSystem: "EPODS",
+            paymentsCount: 36,
+            harvestedDate: "2024-11-19T16:57:24.206",
+          },
+        ],
+      });
+    }
+    return Promise.resolve({});
+  },
 }));
-jest.mock("react-redux", () => ({
-  ...jest.requireActual("react-redux"),
-  useDispatch: jest.fn(),
-}))
