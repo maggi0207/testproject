3ab9ffda969360ad8ebaffc055e231f75088a6b6
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-12-09 20:58:18 -0700

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,34 +1,58 @@
-import { useMemo } from "react";
 
-const isFormValid = useMemo(() => {
-  if (formData.paymentSource === "paymentBroker") {
-    return (
-      formData.dateSetName !== "" &&
-      formData.applicationID !== "" &&
-      formData.startDate !== "" &&
-      formData.endDate !== "" &&
-      formData.startTimeHours !== "" &&
-      formData.endTimeHours !== "" &&
-      formData.startTimeMinutes !== "" &&
-      formData.endTimeMinutes !== ""
-    );
-  }
+import { Page, withModel } from '@adobe/aem-react-editable-components';
+import { Provider } from "react-redux";
+import LoadingIndicator from './components/NextGen/components/LoadingIndicator';
+import store from "./store/index";
+import { setUserActive, setUserInactive } from './store/UserActivity/userActivitySlice';
+import { initializeUserActivityListeners } from './utils/userActivityTracker';
 
-  // Default validation for other paymentSource values
-  return (
-    formData.dateSetName !== "" &&
-    formData.applicationID !== "" &&
-    formData.sourceChannel !== "" &&
-    formData.outboundType !== "" &&
-    formData.startDate !== "" &&
-    formData.endDate !== "" &&
-    formData.startTimeHours !== "" &&
-    formData.endTimeHours !== "" &&
-    formData.startTimeMinutes !== "" &&
-    formData.endTimeMinutes !== ""
-  );
-}, [formData]);
+// This component is the application entry point
+class App extends Page {
 
-useEffect(() => {
-  setIsFormValid(isFormValid);
-}, [isFormValid]);
+    constructor(props) {
+        super(props);
+        console.log("clas props", props);
+        this.activityTimeout = null;
+    }
+
+    componentDidMount() {
+        this.initUserActivityListeners();
+    }
+
+    componentWillUnmount() {
+        this.cleanupUserActivityListeners();
+    }
+
+    initUserActivityListeners = () => {
+        this.cleanupListeners = initializeUserActivityListeners(
+            this.handleUserActive,
+            this.handleUserInactive
+        );
+    }
+
+    cleanupUserActivityListeners = () => {
+        if (this.cleanupListeners) {
+            this.cleanupListeners();
+        }
+    }
+
+    handleUserActive = () => {
+        store.dispatch(setUserActive());
+    }
+
+    handleUserInactive = () => {
+        store.dispatch(setUserInactive());
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+                {this.childComponents}
+                {this.childPages}
+                <LoadingIndicator />
+            </Provider>
+        );
+    }
+}
+
+export default withModel(App);
