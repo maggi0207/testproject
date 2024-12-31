301cbfabb0ce9fbd3251edd4bed8aba8c372a508
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-12-30 23:12:36 -0500

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,1 +1,20 @@
 yyyy
+ const [createApplicationCall, createApplicationCallResponse] = useCreateApplicationMutation();
+import { useCreateApplicationMutation } from 'onevzsoemfecommon/AccountAPIService';
+ useCreateApplicationMutation.mockReturnValue([
+        jest.fn(),
+        {
+          status: 'fulfilled',
+          data: {
+            data: {
+              caseId: 'CCD-13031324-C01',
+              customerInfo: {
+                applicationStatus: 'Application-Initiated',
+                mtn: '4232626151',
+                accountNumber: '0413704717-00001',
+                applicationId: 'U5U5UDT2TQUNTQM4M',
+              },
+            },
+          },
+        },
+      ]);
