53b0921eecf7cdb1117277d2214b0012c221bbcb
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2024-12-31 09:50:10 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,19 +1,25 @@
-jest.mock('onevzsoemfecommon/AccountAPIService', () => ({
-  useCreateApplicationMutation: jest.fn(() => [
-    jest.fn(),
-    {
-      status: 'fulfilled',
-      data: {
+export const useCreateApplicationMutation = () => {
+  if (isMockEnabled) {
+    return [
+      () => Promise.resolve(), // Mock mutation function
+      {
+        status: 'fulfilled',
         data: {
-          caseId: 'CCD-13031324-C01',
-          customerInfo: {
-            applicationStatus: 'Application-Initiated',
-            mtn: '4232626151',
-            accountNumber: '0413704717-00001',
-            applicationId: 'U5U5UDT2TQUNTQM4M',
+          data: {
+            caseId: 'CCD-13031324-C01',
+            customerInfo: {
+              applicationStatus: 'Application-Initiated',
+              mtn: '4232626151',
+              accountNumber: '0413704717-00001',
+              applicationId: 'U5U5UDT2TQUNTQM4M',
+            },
           },
         },
       },
-    },
-  ]),
-}));
+    ];
+  }
+
+  // Actual service import
+  const originalModule = require('onevzsoemfecommon/AccountAPIService');
+  return originalModule.useCreateApplicationMutation();
+};
