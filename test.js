4d58d948c1700acd4eaf04b465b91cef67991bf5
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-01-03 19:39:29 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,10 @@
 const getShippingItemDetails = () => {
-    const storeName = preferredStoreInfo?.storeName;
-    const storeAddressLine1 = preferredStoreInfo?.storeAddressLine1 ?? '';
-    const address1 = preferredStoreInfo?.address1 ?? '';
-    const city = preferredStoreInfo?.city;
-    const state = preferredStoreInfo?.state;
-
+    const { storeName, storeAddressLine1 = '', address1 = '', city, state } = preferredStoreInfo ?? {};
     const storeDetailsText =
       storeName && (storeAddressLine1 || address1) && city && state
         ? `${storeName}, ${storeAddressLine1 || address1}, ${city}, ${state}`
         : "Just bring your devices and we'll take care of the rest. Find a qualified Verizon store near you.";
-
     const visionCustomerType = cartDetails?.cartHeader?.visionCustomerType ?? '';
-
     return {
       storeName,
       storeAddressLine1,
