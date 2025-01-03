734512f40977b7628369ccf82a0c030c772185be
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-01-03 08:33:41 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,16 @@
-const getShippingItemDetails = () => {
-    const { storeName, storeAddressLine1 = '', address1 = '', city, state } = preferredStoreInfo ?? {};
-    const storeDetailsText =
-      storeName && (storeAddressLine1 || address1) && city && state
-        ? `${storeName}, ${storeAddressLine1 || address1}, ${city}, ${state}`
-        : "Just bring your devices and we'll take care of the rest. Find a qualified Verizon store near you.";
-    const visionCustomerType = cartDetails?.cartHeader?.visionCustomerType ?? '';
-    return {
-      storeName,
-      storeAddressLine1,
-      address1,
-      city,
-      state,
-      storeDetailsText,
-      visionCustomerType,
-    };
-};
+export default function ReturnChoice({
+  selectedDevice,
+  mdn,
+  setPreferredStoreId,
+  setPreferredStoreInfo,
+  preferredStoreInfo,
+  chooseTradeInSelected,
+  updateChooseTradeInSelected,
+  setEnableContinue,
+  setCurrentSection,
+  selectedTradeStoreId,
+  setDefaultZipCode,
+  cartDetails,
+  isEnabledTradeReturnOptionsInContactCenterApps,
+  customerType
+})
