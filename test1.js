c2e6709983497713fe8a5f094c7ea0d885cfcae4
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-01-07 09:13:22 +0530

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,19 +1,19 @@
 import React from "react";
-import {Snackbar, Box, Typography, Grid} from "@material-ui/core";
+import { Snackbar, Box, Typography, Grid } from "@material-ui/core";
 
-const ToastNotification = ({open, handleClose, message, type, heading, fromApi}) => {
+const ToastNotification = ({ open, handleClose, message, type, heading, fromApi }) => {
     return (
         <Snackbar
             open={open}
             autoHideDuration={6000}
             onClose={handleClose}
             style={{
-                position : "fixed",
-                top : fromApi === 'updateEmailDelivery' ? "130px" : "330px",
-                right : "30px",
-                zIndex : 9999,
+                position: "fixed",
+                top: fromApi === 'updateEmailDelivery' ? "130px" : "330px",
+                right: "30px",
+                zIndex: 9999,
             }}
-            anchorOrigin={{vertical: "top", horizontal: "right"}}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
         >
             <Box className={`toast-notification ${type === "success" ? "success" : "failure"}`}>
                 <Grid container spacing={1}>
@@ -27,14 +27,15 @@ const ToastNotification = ({open, handleClose, message, type, heading, fromApi})
                         </Box>
                     </Grid>
 
-
                     <Grid item xs>
                         <Typography variant="h6" className="heading">
-                            {heading || (type === "success" ? "Success" : "Failure")}
+                            {heading || message} {/* Show message if heading is not defined */}
                         </Typography>
-                        <Typography variant="body2" className="message">
-                            {message}
-                        </Typography>
+                        {heading && (
+                            <Typography variant="body2" className="message">
+                                {message}
+                            </Typography>
+                        )}
                     </Grid>
                 </Grid>
             </Box>
