09dc58949db8bf8c36616492024f0dc652bf198b
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-01-19 21:51:02 -0700

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,1 +1,14 @@
-<Box sx={{ marginBottom: 2 }}> <Typography variant="h6" sx={{ fontWeight: 'bold', textAlign: 'left' }}> Default Par Roll Sort </Typography> </Box>
+{"pathSuffix": "udlm",
+    "requestObj": {
+        "ordered_update_names": ["* Payer Processing Issues", "Assurance Reimbursement Management", "Clearance Patient Access Suite","Revenue Performance Advisor", "Testing List"],
+        "ordered_create_names": ["Exchange", "Intelligent EDI"],
+        "* Payer Processing Issues": "Ecc Hub Notification",
+        "Assurance Reimbursement Management": "Email",
+        "Clearance Patient Access Suite": "Email;ECC Hub Notification",
+        "Revenue Performance Advisor": "",
+        "Testing List": "",
+        "Exchange": "Email",
+        "Intelligent EDI": "Email"
+
+  }
+}
