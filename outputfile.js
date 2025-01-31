68edbc8223013a6aad630814731d390339c9404d
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-01-30 22:32:06 -0700

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,11 +1,410 @@
-const getSelectedItemCancelStatus = (selected) => {
-    const filteredOrders = orderDetailsWithoutError.filter(item => selected.includes(item.mtn));
-    const filterPendingOrderItems = filteredOrders.flatMap(order => 
-        (order.pendingOrderDetail || []).filter(detail => detail.pendingOrderNumber === props.pendingOrderId)
-    );
-    return canCancelOrder(filterPendingOrderItems);
-};
- {selected.length < orderDetailsWithoutError.length && <StyledButton data-track='service change_delete item' isDark={props.isDark} data-testid='deleteItem' disabled={!getSelectedItemCancelStatus(selected) && (selected.length === 0 || isALPtoLLPChange)} secondary onClick={deleteItem}>Delete Item</StyledButton>}
-                     {selected.length === orderDetailsWithoutError.length && <StyledButton  data-track='service change_delete order ' isDark={props.isDark} data-testid='deleteOrder' disabled={!getSelectedItemCancelStatus(selected) && selected.length === 0} secondary onClick={deleteOrder}>Delete Order</StyledButton>}
-                 </div>
+"vehicles": [
+        {
+            "interest_list": [
+                {
+                    "name": "",
+                    "location": {
+                        "zip": "",
+                        "address_line_1": "",
+                        "country": "",
+                        "address_line_2": "",
+                        "state": "",
+                        "city": ""
                     }
+                }
+            ],
+            "ubi_eligibility": {
+                "is_eligible": true,
+                "sub_type": "CAR",
+                "telematics_channel": {
+                    "channel_id": "1002",
+                    "channel_name": "Stellantis"
+                },
+                "type": "NSP"
+            },
+            "usage": {
+                "annual_mileage": {
+                    "number": "10000"
+                },
+                "primary_use": "Commute"
+            },
+            "description": {
+                "vehicle_symbols": {
+                    "model_symbol": "3C",
+                    "auxiliary_symbol": "XX",
+                    "symbol_dataset_version": "az_s2024_vehicle_symbols",
+                    "make_symbol": "CY",
+                    "style_symbol": "46"
+                },
+                "year": "2018",
+                "cylinder_count": "6",
+                "model": "300 LIMITED",
+                "style": "Sedan 4 DR",
+                "gross_vehicle_weight": "",
+                "make": "CHRYSLER",
+                "door_count": "4"
+            },
+            "order_status": {
+                "description": "Ordered",
+                "code": "O"
+            },
+            "source": "user",
+            "is_from_make_model_year": false,
+            "feature_list": {
+                "is_blind_spot_warning": null,
+                "is_automatic_emergency_braking": null,
+                "is_convertible": {
+                    "value": "N",
+                    "source": "user"
+                }
+            },
+            "license_plate": {
+                "id": "",
+                "state": "TN"
+            },
+            "ownership": {
+                "ownership_length": {
+                    "description": "1-5 months",
+                    "code": "F"
+                },
+                "ownership_status": "Own"
+            },
+            "msrp": {
+                "value": 15950,
+                "source": "prefill"
+            },
+            "is_occupied_vin": false,
+            "is_valid_standard_auto": false,
+            "vin": "2C3CCAKG7JH211430",
+            "vehicle_id": "541fff46-e5d3-4d69-a150-9ff65452fca0",
+            "severe_accident_list": [],
+            "garaging_address": {
+                "zip": "85339",
+                "address_line_1": "",
+                "country": "",
+                "address_line_2": "",
+                "state": "",
+                "city": ""
+            },
+            "is_ride_sharing": false,
+            "is_included": true,
+            "title_brand_list": []
+        },
+        {
+            "interest_list": [
+                {
+                    "name": "",
+                    "location": {
+                        "zip": "",
+                        "address_line_1": "",
+                        "country": "",
+                        "address_line_2": "",
+                        "state": "",
+                        "city": ""
+                    }
+                }
+            ],
+            "ubi_eligibility": {
+                "type": "NSP",
+                "is_eligible": false,
+                "telematics_channel": {
+                    "channel_id": ""
+                }
+            },
+            "usage": {
+                "annual_mileage": {
+                    "number": "10000"
+                },
+                "primary_use": "Business"
+            },
+            "description": {
+                "vehicle_symbols": {
+                    "model_symbol": "3C",
+                    "auxiliary_symbol": "XX",
+                    "symbol_dataset_version": "az_s2024_vehicle_symbols",
+                    "make_symbol": "CY",
+                    "style_symbol": "48"
+                },
+                "year": "2006",
+                "cylinder_count": "8",
+                "model": "300 C",
+                "style": "Sedan 4 DR",
+                "gross_vehicle_weight": "",
+                "make": "CHRYSLER",
+                "door_count": "4"
+            },
+            "order_status": {
+                "description": "Ordered",
+                "code": "O"
+            },
+            "source": "user",
+            "is_from_make_model_year": false,
+            "feature_list": {
+                "is_blind_spot_warning": null,
+                "is_automatic_emergency_braking": null,
+                "is_convertible": {
+                    "value": "N",
+                    "source": "user"
+                }
+            },
+            "license_plate": {
+                "id": "",
+                "state": "WY"
+            },
+            "ownership": {
+                "ownership_length": {
+                    "description": "1-5 months",
+                    "code": "F"
+                },
+                "ownership_status": "Own"
+            },
+            "msrp": {
+                "value": 3310,
+                "source": "prefill"
+            },
+            "is_occupied_vin": false,
+            "is_valid_standard_auto": false,
+            "vin": "2C3LA63H16H171816",
+            "vehicle_id": "c1935d2b-b69f-4760-a7d4-a3a0a6d76275",
+            "severe_accident_list": [],
+            "garaging_address": {
+                "zip": "85339",
+                "address_line_1": "",
+                "country": "",
+                "address_line_2": "",
+                "state": "",
+                "city": ""
+            },
+            "is_ride_sharing": false,
+            "is_included": true,
+            "title_brand_list": []
+        },
+        {
+            "interest_list": [
+                {
+                    "name": "",
+                    "location": {
+                        "zip": "",
+                        "address_line_1": "",
+                        "country": "",
+                        "address_line_2": "",
+                        "state": "",
+                        "city": ""
+                    }
+                }
+            ],
+            "ubi_eligibility": {
+                "type": "",
+                "is_eligible": false
+            },
+            "usage": {
+                "annual_mileage": {
+                    "number": "10000"
+                },
+                "primary_use": "Business"
+            },
+            "description": {
+                "vehicle_symbols": {
+                    "model_symbol": "TG",
+                    "auxiliary_symbol": "BJ",
+                    "symbol_dataset_version": "az_s2024_vehicle_symbols",
+                    "make_symbol": "AC",
+                    "style_symbol": "44"
+                },
+                "year": "2023",
+                "cylinder_count": "4",
+                "body_style": "4 DOOR SEDAN 4 cyl",
+                "model": "INTEGRA",
+                "style": "4 DOOR SEDAN",
+                "gross_vehicle_weight": "",
+                "make": "ACURA",
+                "door_count": ""
+            },
+            "source": "user",
+            "is_from_make_model_year": true,
+            "feature_list": {
+                "is_blind_spot_warning": null,
+                "is_automatic_emergency_braking": null,
+                "is_convertible": {
+                    "value": "N",
+                    "source": "user"
+                }
+            },
+            "ownership": {
+                "ownership_length": {
+                    "description": "1-5 months",
+                    "code": "F"
+                },
+                "ownership_status": "Own"
+            },
+            "msrp": {
+                "value": 0,
+                "source": "user"
+            },
+            "vin": "HNHF0WM01ZE7NC880",
+            "vehicle_id": "95afa1d9-835f-4159-aa97-317e6f1a6b24",
+            "garaging_address": {
+                "zip": "85339",
+                "address_line_1": "",
+                "country": "",
+                "address_line_2": "",
+                "state": "",
+                "city": ""
+            },
+            "is_ride_sharing": false,
+            "is_included": true
+        }
+    ],
+    "drivers": [
+        {
+            "personal_detail": {
+                "name": {
+                    "first_name": "Useronedriver",
+                    "last_name": "One Driver",
+                    "middle_name": "",
+                    "suffix": ""
+                },
+                "address": {
+                    "zip": "85339",
+                    "address_line_1": "9850 S 65th Ave",
+                    "country": "US",
+                    "address_line_2": "",
+                    "state": "AZ",
+                    "city": "Laveen Village"
+                },
+                "gender": "Male",
+                "dob": "1994-09-09",
+                "marital_status": {
+                    "description": "Married",
+                    "code": "MA",
+                    "label": "Married"
+                }
+            },
+            "education": {
+                "description": "Vocational/trade school degree or military training",
+                "code": "3",
+                "label": "Vocational/trade school degree or military training"
+            },
+            "occupation": {
+                "code": "ABL",
+                "category_code": "ART"
+            },
+            "contact_info": {
+                "phone": [
+                    {
+                        "type": "cell",
+                        "number": "9769697978",
+                        "is_primary": true
+                    }
+                ],
+                "email": "testkbkbk@gmail.com"
+            },
+            "source": "user",
+            "relation_to_pni": "self",
+            "driver_type": "primary",
+            "accident_prevention_course_date": "",
+            "had_driver_training": false,
+            "license": {
+                "age_license_obtained": "18",
+                "state": "AZ",
+                "id": "324234242",
+                "type": "Personal",
+                "status": "Active/Valid"
+            },
+            "driver_id": "133b02d7-15f2-4959-a66f-7c4a48aa5795",
+            "driving_history": {
+                "incidents": {
+                    "from_user": []
+                }
+            },
+            "prior_insurance": {
+                "has_auto_insurance_today": true
+            },
+            "student_profile": {
+                "is_good_student": false,
+                "is_distant_student": false,
+                "is_enrolled_student": false
+            },
+            "risk_check_attribute": {
+                "risk_check_key": "useronedriver-one-driver-19940909",
+                "risk_check_score_summary": {
+                    "credit_header": "N",
+                    "score_decile": "10",
+                    "risk_group": "VERY HIGH",
+                    "score_color": "RED",
+                    "total_score": "5211",
+                    "is_cra_warning": false,
+                    "is_fraud_indicator": false
+                }
+            },
+            "residence": {
+                "resident_number": "2",
+                "residence_type": "Own home",
+                "is_mailing_address_same_as_primary_address": true,
+                "location": {},
+                "is_home_owner": true,
+                "is_pobox_or_military_address": false
+            },
+            "safety_training_course_date": ""
+        },
+        {
+            "personal_detail": {
+                "name": {
+                    "first_name": "Userdriverone",
+                    "last_name": "Kjbkjbjkb",
+                    "middle_name": "",
+                    "suffix": ""
+                },
+                "gender": "Female",
+                "dob": "1996-09-09",
+                "marital_status": {
+                    "description": "Married",
+                    "code": "MA",
+                    "label": "Married"
+                }
+            },
+            "education": {
+                "description": "Unknown",
+                "code": "X",
+                "label": "Unknown"
+            },
+            "occupation": {
+                "code": "020",
+                "category_code": "RET"
+            },
+            "contact_info": {
+                "phone": [
+                    {
+                        "type": "cell",
+                        "is_primary": true
+                    }
+                ]
+            },
+            "spouse_included": true,
+            "source": "user",
+            "relation_to_pni": "Spouse",
+            "driver_type": "Included",
+            "license": {
+                "age_license_obtained": "20",
+                "state": "AZ",
+                "id": "809809808",
+                "type": "Personal",
+                "status": "Active/Valid"
+            },
+            "driver_id": "f30d94cb-4e92-432b-8e21-08a3c540621a",
+            "driving_history": {
+                "incidents": {
+                    "from_user": []
+                }
+            },
+            "prior_insurance": {
+                "has_auto_insurance_today": true
+            },
+            "student_profile": {
+                "is_good_student": false,
+                "is_distant_student": false,
+                "is_enrolled_student": false
+            },
+            "safety_training_course_date": ""
+        }
+    ],
