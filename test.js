182c758eaadd652c761e5022325609b2915e624a
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-01-31 00:08:54 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,86 +1,169 @@
+
 .quote-summary-container {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 24px;
-  gap: 24px;
-  position: absolute;
-  width: 628px;
-  height: 874px;
-  left: calc(50% - 314px);
-  top: 56px;
-  font-family: 'Lato', sans-serif;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 24px;
+    gap: 24px;
+    position: absolute;
+    width: 628px;
+    height: 874px;
+    left: calc(50% - 314px);
+    top: 56px;
+    font-family: 'Lato', sans-serif;
 }
 
 .quote-summary-content {
-  text-align: left;
-  width: 580px;
-  padding: 8px;
-  gap: 8px;
+
+    text-align: left;
+    width: 580px;
+    padding: 8px;
+    gap: 8px;
 }
 
 .quote-summary-paper {
-  padding: 12px;
-  gap: 12px;
-  background: #ffffff;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
+    padding: 12px;
+    gap: 12px;
+    background: #ffffff;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
 }
 
 .quote-summary-title {
-  font-size: 24px;
-  font-weight: 700;
-  line-height: 150%;
-  color: #000000;
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 150%;
+    color: #000000;
 }
 
 .quote-summary-subtitle {
-  font-size: 18px;
-  font-weight: 500;
-  line-height: 160%;
-  color: #000000;
+
+    font-family: Lato;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 28.8px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+
+}
+
+.quote-summary-price-section {
+
+    margin-top: 24px;
 }
 
 .quote-summary-text {
-  font-size: 18px;
-  font-weight: 500;
-  line-height: 160%;
-  color: #000000;
+    font-family: Lato;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 28.8px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+
 }
 
 .quote-summary-price {
-  font-size: 18px;
-  font-weight: bold;
-  color: #000000;
+
+    font-family: Rubik;
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 36px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+
+
+}
+
+.quote-summary-price-discount {
+    font-family: Lato;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 19.6px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+    color: #7300FF
 }
 
 .quote-summary-divider {
-  margin: 12px 0;
-  border: 1px solid #b9c0ca;
+    margin-top: 24px;
+    margin-bottom: 24px;
+    border: 1px solid #b9c0ca;
 }
 
 .quote-summary-section {
-  font-size: 18px;
-  font-weight: 700;
-  color: #000000;
+    //styleName: Theme Styles/body-lg;
+    font-family: Lato;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 28.8px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+
+    padding-bottom: 12px;
+    color: #000000;
 }
 
 .quote-summary-card {
-  padding: 12px;
-  gap: 12px;
-  border: 1px solid #b9c0ca;
-  border-radius: 8px;
+    padding: 12px;
+    gap: 12px;
+    border: 1px solid #b9c0ca;
+    border-radius: 8px;
 }
 
 .quote-summary-subheading {
-  font-size: 18px;
-  font-weight: 700;
-  color: #000000;
+
+    font-family: Lato;
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 28.8px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+    padding-bottom: 12px;
+
+}
+
+.quote-summary-subheading-title {
+
+    font-family: Lato;
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 28.8px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+
 }
 
 .quote-summary-driver,
 .quote-summary-vehicle {
-  font-size: 18px;
-  font-weight: 500;
-  color: #40475e;
+    //styleName: Theme Styles/body-sm;
+    font-family: Lato;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 19.6px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+
+    color: #40475e;
+}
+
+.quote-vechile-info-section{
+    display: flex;
+    gap: 12px;
+    flex-direction: column;
+    margin-bottom: 12px;
+}
+
+.quote-driver-info-section{
+    display: flex;
+    gap: 4px;
+    flex-direction: column;
+    margin-bottom: 12px;
 }
