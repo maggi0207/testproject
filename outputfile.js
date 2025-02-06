88492c417b3093b1d61003f7894144030ed1ab59
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-02-06 18:52:17 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,15 +1,29 @@
-<Select
-          isMulti
-          options={options}
-          value={tempSelection}
-          onChange={setTempSelection}
-          className="select-box"
-          placeholder="Select Options"
-          closeMenuOnSelect={false}
-          isClearable
-          hideSelectedOptions={false}
-          getValue={() => tempSelection.length > 1 
-            ? [{ label: `${tempSelection.length} items selected`, value: "count" }] 
-            : tempSelection
-          }
-        />
+import React from "react";
+import { Modal, Button } from "@mui/material";
+import "./RegisterUserModal.scss";
+
+const RegisterUserModal = ({ isOpen, onClose, header, content, buttons }) => {
+  return (
+    <Modal open={isOpen} onClose={onClose} className="modal-container">
+      <div className="modal-content">
+        <h2 className="modal-header">{header}</h2>
+
+        <p className="modal-body">{content}</p>
+
+        <div className="modal-footer">
+          {buttons?.map((button, index) => (
+            <Button
+              key={index}
+              onClick={button.onClick}
+              variant={button.primary ? "contained" : "outlined"}
+            >
+              {button.label}
+            </Button>
+          ))}
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default RegisterUserModal;
