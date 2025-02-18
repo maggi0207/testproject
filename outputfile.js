3f45bbf1c51c2fa5a071049e7ca257be6c38a020
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-02-18 09:30:01 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,29 +1,3 @@
-import React from "react";
-import { Modal, Button } from "@mui/material";
-import "./RegisterUserModal.scss";
-
-const RegisterUserModal = ({ isOpen, onClose, header, content, buttons }) => {
-  return (
-    <Modal open={isOpen} onClose={onClose} className="modal-container">
-      <div className="modal-content">
-        <h2 className="modal-header">{header}</h2>
-
-        <p className="modal-body">{content}</p>
-
-        <div className="modal-footer">
-          {buttons?.map((button, index) => (
-            <Button
-              key={index}
-              onClick={button.onClick}
-              variant={button.primary ? "contained" : "outlined"}
-            >
-              {button.label}
-            </Button>
-          ))}
-        </div>
-      </div>
-    </Modal>
-  );
-};
-
-export default RegisterUserModal;
+"scripts": {
+  "build": "babel src/index.js --out-file dist/index.cjs.js --presets=@babel/preset-env && terser dist/index.cjs.js --compress --mangle --output dist/index.cjs.min.js"
+}
