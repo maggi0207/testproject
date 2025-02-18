b83ad0bd06822e3d97fca2367d8f59a9b7da57b6
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-02-18 09:34:09 +0530

    Update outputfile.js

--- outputfile.js
+++ outputfile.js
@@ -1,3 +1,1 @@
-"scripts": {
-  "build": "babel src/index.js --out-file dist/index.cjs.js --presets=@babel/preset-env && terser dist/index.cjs.js --compress --mangle --output dist/index.cjs.min.js"
-}
+"build": "babel src --out-file dist/index.cjs.js --presets=@babel/preset-env && terser dist/index.cjs.js --compress --mangle --output dist/index.cjs.min.js"
