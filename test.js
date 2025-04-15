d9356b689216f43da373b43d832e09769a7ead26
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-04-15 14:18:05 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,13 @@
 const isPaymentTypeAC = paymentDetails?.some((payment) => payment.modeOfPay === 'AC');
 
-  let signatureRequired;
-  // #3
-  if (isPrePayCart && channel?.includes('OMNI-INDIRECT')) {
-    signatureRequired = true;
-  } else if (channel?.includes('OMNI-RETAIL') && isPaymentTypeAC && !isRFEXNegativePayment) {
-    signatureRequired = true;
-  } else {
-    signatureRequired =
-      agreementEligibleFlags?.showSignatureRequired ||
-      isWHWFlow ||
-      (channel === 'OMNI-CARE' && agreementEligibleFlags?.showNetworkExtenderTnCDiscountApplied);
-  }
+const isOmniIndirect = isPrePayCart && channel?.includes('OMNI-INDIRECT');
+const isOmniRetailWithAC = channel?.includes('OMNI-RETAIL') && isPaymentTypeAC && !isRFEXNegativePayment;
+const isOmniCareWithDiscount =
+  channel === 'OMNI-CARE' && agreementEligibleFlags?.showNetworkExtenderTnCDiscountApplied;
+
+const signatureRequired =
+  isOmniIndirect ||
+  isOmniRetailWithAC ||
+  agreementEligibleFlags?.showSignatureRequired ||
+  isWHWFlow ||
+  isOmniCareWithDiscount;
