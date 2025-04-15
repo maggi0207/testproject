ae007ac31e88f9a70ac45b289b58fda00db1e540
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-04-15 02:00:45 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,43 @@
-jest.mock('@vz-soe-utils/SigPad', () => {
-  const b64 = 'data:image/png;base64,' + 'iVBORw0KGgoAAAANSUhEUgAAA7gAAACWCAYAAAD0dySAAA'.padEnd(6000, 'A');
-
-  return {
-    __esModule: true,
-    default: ({ signatureHandler }) => (
-      <div>
-        <button
-          type="button"
-          data-testid="mocked-sigpad"
-          onClick={() => signatureHandler(b64, true)}
-        >
-          sigpadMockBtn
-        </button>
-      </div>
-    ),
+ const invokeReceivePoll = async (eventData) => {
+    handleSingnatrueReceivePoll(
+      eventData,
+      clientId,
+      cartDetails,
+      customerProfileData,
+      submitSignatureAPI,
+      requestBodySendMsg,
+      VTZDSubmitPayment,
+      VTSetIPAddress,
+      zeroDollarOrder,
+      singnatureReceiveCallBack
+    );
   };
-});
+
+  const VTZDSubmitPayment = (signatureReqBody, makePaymentReqBody) => {
+    const custIP = sessionStorage.getItem('customerIPAddress') ? sessionStorage.getItem('customerIPAddress') : '';
+    const newSignatureReqBody = { ...signatureReqBody };
+    newSignatureReqBody.ipAddress = custIP;
+    setSignatrueRquestBody(newSignatureReqBody);
+    setMakePaymentBody(makePaymentReqBody);
+    console.log(makePaymentBody);
+    const body = {
+      ipAddress: custIP,
+      paymentType: 'ZD',
+      paymentAmount: 0,
+      customerInfo: {
+        processingMTN: cartHeader?.cartCreator,
+        cartCreator: cartHeader?.cartCreator,
+      },
+      regNo: '51',
+      isIndirectBuyOut: false,
+      orderNumber,
+      isIpad,
+    };
+
+    if (signatureReqBody?.safeTechSessionId) {
+      body.safeTechSession = 'true';
+      body.safeTechSessionId = signatureReqBody?.safeTechSessionId;
+    }
+
+    submitPayment({ body, spinner: true, mock: false });
+  };
