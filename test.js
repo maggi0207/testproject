4a637a3a19d416592a23e17176ddf9a0e96713e8
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-04-15 12:06:18 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,18 @@
-const b64 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA7gAAACWCAYAAAD0dySAAA';
+jest.mock('@vz-soe-utils/SigPad', () => {
+  const b64 = 'data:image/png;base64,' + 'iVBORw0KGgoAAAANSUhEUgAAA7gAAACWCAYAAAD0dySAAA'.padEnd(6000, 'A');
 
-jest.mock('@vz-soe-utils/SigPad', () => ({
-  __esModule: true,
-  default: ({ signatureHandler }) => (
-    <div>
-      <button type='button' data-testid='mocked-sigpad' onClick={() => signatureHandler(b64, true)}>
-        sigpadMockBtn
-      </button>
-    </div>
-  ),
-}));
+  return {
+    __esModule: true,
+    default: ({ signatureHandler }) => (
+      <div>
+        <button
+          type="button"
+          data-testid="mocked-sigpad"
+          onClick={() => signatureHandler(b64, true)}
+        >
+          sigpadMockBtn
+        </button>
+      </div>
+    ),
+  };
+});
