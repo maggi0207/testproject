bc64810afb3257a545939d939fcb7b6c4ca077c7
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2025-07-24 10:30:43 +0530

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,31 +1,39 @@
-// vehiclePrefillStore.ts
+import useVehiclePrefillStore from './vehiclePrefillStore';
+import { renderHook, act } from '@testing-library/react-hooks';
 
-import { create } from 'zustand';
-import { devtools, persist, createJSONStorage } from 'zustand/middleware';
+describe('Zustand vehiclePrefillStore', () => {
+  beforeEach(() => {
+    sessionStorage.clear();
+    useVehiclePrefillStore.getState().reset(); // force store reset
+  });
 
-export const initialValues = {
-  vehiclePrefills: [],
-};
+  it('should initialize with empty vehiclePrefills', () => {
+    const { result } = renderHook(() => useVehiclePrefillStore());
+    expect(result.current.vehiclePrefills).toEqual([]);
+  });
 
-type VehiclePrefillStore = {
-  vehiclePrefills: any[];
-  setVehiclePrefills: (data: any[]) => void;
-};
+  it('should update vehiclePrefills using setVehiclePrefills', () => {
+    const { result } = renderHook(() => useVehiclePrefillStore());
 
-const useVehiclePrefillStore = create<VehiclePrefillStore>()(
-  devtools(
-    persist(
-      (set) => ({
-        ...initialValues,
-        setVehiclePrefills: (vehiclePrefills) =>
-          set({ vehiclePrefills }, false, 'setVehiclePrefills'),
-      }),
-      {
-        name: 'vehicle-prefills-store',
-        storage: createJSONStorage(() => sessionStorage),
-      }
-    )
-  )
-);
+    const testPrefills = [{ make: 'Tata', model: 'Nexon' }];
 
-export default useVehiclePrefillStore;
+    act(() => {
+      result.current.setVehiclePrefills(testPrefills);
+    });
+
+    expect(result.current.vehiclePrefills).toEqual(testPrefills);
+  });
+
+  it('should persist vehiclePrefills to sessionStorage', () => {
+    const { result } = renderHook(() => useVehiclePrefillStore());
+
+    const testPrefills = [{ make: 'Hyundai', model: 'i20' }];
+
+    act(() => {
+      result.current.setVehiclePrefills(testPrefills);
+    });
+
+    const persisted = JSON.parse(sessionStorage.getItem('vehicle-prefills-store'));
+    expect(persisted?.state?.vehiclePrefills).toEqual(testPrefills);
+  });
+});
