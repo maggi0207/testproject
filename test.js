736bb4f4fc29bf43e1955ce0c4b5c1fb6ebd866d
maggi0207 <156363912+maggi0207@users.noreply.github.com>
2026-01-28 10:24:26 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,43 +1,30 @@
-flowchart LR
+---
+config:
+  layout: fixed
+---
+flowchart TB
+ subgraph HORIZ[" "]
+    direction LR
+        NR1["New Route in M1V2 for Resume"]
+        NR2["New Route to SVC"]
+  end
+    SQ(["Saved Quotes Hub"]) --> QT{"Quote Type"}
+    QT -.-> spacer1(["ACQ"]) & spacer2(["SVC"])
+    spacer1 --> NR1
+    spacer2 --> NR2
+    NR1 --> HC1["Trigger resume<br>API from ACQ launcher exp<br>and show interstitial loading indicator"]
+    HC1 --> DEC1{"If premium<br>Changed OR<br>COPI / Risk Code<br>changed"}
+    DEC1 -- Yes --> SB1["Display Speed Bump<br>if conditions are met"]
+    DEC1 -- No --> NSB1["Do Not Display<br>Speed Bump"]
+    SB1 --> QD1{"If Quoted"}
+    NSB1 --> QD1
+    QD1 -- Yes --> CWQ1(["Continue with Quoted Flow"])
+    QD1 -- No --> CWQH1(["Continue without Quoted Handling"])
+    NR2 --> HC2["Other quote handling"]
+    HC2 --> CWQH2(["Continue without Quoted Handling"])
 
-%% ================= LEFT SECTION =================
-A[Saved Quotes<br/>Hub]
-
-B{Quote Type}
-
-A --> B
-
-B -->|ACQ| C[New Route in<br/>M1V2 for Resume]
-
-C --> D[Hook changes to<br/>
-pull resume End<br/>
-Point from ACQ<br/>
-launcher exp and<br/>
-show interstitial]
-
-%% ================= DECISION =================
-D --> E{If premium<br/>
-Changed OR<br/>
-COPI / Risk Code<br/>
-changed}
-
-%% ================= YES PATH (UP) =================
-E -->|Yes| F[Display Speed<br/>
-Bump based on<br/>
-Speed bump<br/>
-conditions]
-
-%% ================= NO PATH (RIGHT) =================
-E -->|No| G[Do Not Display<br/>
-Speed Bump]
-
-%% ================= ALIGNMENT HELPERS =================
-F --> H{If Quoted}
-G --> H
-
-%% ================= OUTCOMES =================
-H -->|Yes| I[Continue with<br/>
-Quoted Flow]
-
-H -->|No| J[Continue without<br/>
-Quoted Handling]
+     QT:::decision
+     DEC1:::decision
+     QD1:::decision
+    classDef decision fill:#F6D776,stroke:#BB8F0B,stroke-width:2px
+    style spacer1 color:#000000
